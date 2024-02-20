6b2ec62d905e872ba16b3763ff69365aa08b8f63
Rashay01 <101921464+Rashay01@users.noreply.github.com>
2024-02-20 10:07:34 +0000

    Exercises completed

--- string_ex.py
+++ string_ex.py
@@ -8,7 +8,6 @@ index = message.find(key)
 
 output = message[index+1:].upper() if index >= 0 else "No secrect is preset"
 
-#print(output)
 if(output == code):
   print("You are an hacker")
 
@@ -20,19 +19,39 @@ else:
 
 message = "    🚨🔍📱secret_code✌️"
 code = "SECRET_CODE✌️"
-error_nokey = "No secrect is preset"
+error_nokey = "No, secrect is preset"
 
 #strat
 key = "🔑"
 
 index = message.find(key)
 
-output = message[index+1:].upper() if index >= 0 else "No secrect is preset"
+output = message[index+1:].upper() if index != -1 else print("No, secrect is preset")
 
-#print(output)
 if(output == code):
   print("You are an hacker")
-elif (output == error_nokey):
-  print(output)
+else:
+  print("try again")
+
+#===================================================================================
+#Task3
+
+message = "    🚨🔍📱🔑((((****secret_code✌️(((((*****"
+code = "SECRET_CODE✌️"
+error_nokey = "No, secrect is preset"
+
+#strat
+key = "🔑"
+
+index = message.find(key)
+
+output = message[index+1:].upper() if index != -1 else print("No, secrect is preset")
+
+output = str(output).strip("(")
+output = str(output).strip("*")
+
+if(output == code):
+  print("You are an hacker")
+
 else:
   print("try again")

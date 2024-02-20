e4a0f8e65879f5059ebef17933be73d6dd583b73
Rashay01 <101921464+Rashay01@users.noreply.github.com>
2024-02-20 07:41:30 +0000

    Conditional added

---
+++ conditional.py
@@ -0,0 +1,14 @@
+# lexical order
+
+#get two people heights 
+person1 = input("Please provide your name: ")
+height1 = float(input("Please provide your height in cm: "))
+person2 = input("Please provide your name: ")
+height2 = float(input("Please provide your height in cm: "))
+
+if (height1 > height2):
+  print(f"{person1} is taller")
+elif (height1 == height2):
+  print(f"{person1} and {person2} are of same height")
+else:
+  print(f"{person2} is taller")

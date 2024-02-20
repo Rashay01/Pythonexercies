8ec75c7588a01922b99f9a0ce9b21117340bda79
Rashay01 <101921464+Rashay01@users.noreply.github.com>
2024-02-20 07:16:38 +0000

    Day 2 Initilisation

--- main.py
+++ main.py
@@ -35,18 +35,4 @@
 # empty = " " * (10 - int(progress))
 # print(f"[{bar}{empty}] {percentage}%")
 
-# substring
-
-quote ="I love python"
-#splicing operater
-# print(quote[start:end]) print(quote[inclusive:exclusive]) 
-
-# quote[2] = 'x' ERROR it is immutable
-
-# print(quote[2:6])
-# print(quote[-1])
-print(quote[-6:])
-print(quote[::2])
-print(quote[::-1])
-
-x = None #NUll initilisation  
+x = None #NUll initilisation 
---
+++ slice.py
@@ -0,0 +1,15 @@
+# substring
+
+quote ="I love python"
+#splicing operater
+# print(quote[start:end]) print(quote[inclusive:exclusive]) 
+
+# quote[2] = 'x' ERROR it is immutable
+
+# print(quote[2:6])
+# print(quote[-1])
+print(quote[-6:])
+print(quote[::2])
+print(quote[::-1])
+
+ 

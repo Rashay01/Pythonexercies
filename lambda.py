6fd94327faa0e320a880d5907fffecad09d22836
Rashay01 <101921464+Rashay01@users.noreply.github.com>
2024-02-22 16:12:21 +0000

    lambda notes

--- lambda.py
+++ lambda.py
@@ -3,7 +3,7 @@
 print("Lamda functions:")
 
 
-def sum(a, b):
+def sum1(a, b):
   return a + b
 
 
@@ -68,3 +68,59 @@ print(map_own(lambda x: x * 2, arr))
 print(arr)
 
 #map object -> lazy | efficent
+
+
+#3
+def sayHello1():
+  def msg():
+    return "hello, 🎊"
+  return msg
+
+print(sayHello1()())
+
+#implicit return - lambda x returns lambda y | factory of functions
+#curring, Partials - Assignment
+#Functional programming - F#, Herskel (no loops)
+# Curring, HOF, Recursion
+#OOP programing - inheritence
+#Procedural programming
+#Mathematical programming 
+
+mul = lambda x: lambda y: x*y 
+
+print(mul(3)(6))
+
+
+
+#HOF - argument is a function
+result1 = map(lambda x: x * 2,[10, 30, 60])
+result2 = filter(lambda x:x >10, [10, 50, 60, 100, 6, 8 ,30])
+print(list(result2))
+
+# Pythonic way 
+print(sum([10,30,60]))
+
+#Sequence - List,
+#1. len()
+#2. sum()
+#3. sorted()
+#4. max()
+#5. min()
+
+print(max([10,30,60]))
+print(min([10,30,60]))
+
+print(all([True, False, True])) #all
+print(any([True, False, True])) # or
+
+print(all([10,0,-1])) 
+# falsy values| converted to Boolean it is False -> 0, None, [], {}, set(), "", ()
+# Other values are Truthly
+
+x=[] 
+#if requires a boolean 
+
+if(x):
+  print("cool")
+else:
+  print("super")

418b5077c16cda69cbfe0e31d738a15723d0a95b
Rashay01 <101921464+Rashay01@users.noreply.github.com>
2024-02-22 18:07:12 +0000

    Final edits

--- hotel_booking_system.py
+++ hotel_booking_system.py
@@ -1,7 +1,38 @@
+#Task1
+def add_room(rooms, room_number, bed_type="Double", smoking=False):
+  for room in rooms:
+    if (room["room_number"] == room_number):
+      room["bed_type"] = bed_type
+      room["smoking"] = smoking
+      return
+  rooms.append({
+      "room_number": room_number,
+      "bed_type": bed_type,
+      "smoking": smoking,
+      "availability": True
+  })
+
+
+#Task2
+def book_room(rooms, preferred_bed_type="Double", smoking_preference=False):
+  for room in rooms:
+    if (room["bed_type"] == preferred_bed_type
+        and room["smoking"] == smoking_preference and room["availability"]):
+      room["availability"] = False
+      return "room successfully booked"
+  return f"rooms with {preferred_bed_type} and smoking: {smoking_preference} fully booked"
+
+
+#Task3
+def list_available_rooms(rooms):
+  return [room for room in rooms if room["availability"]]
+
+
+#-----------------------------------------Testing----------------------------------------------
 rooms = [{
     "room_number": 1,
     "bed_type": "Queen",
-    "smoking": False,
+    "smoking": True,
     "availability": True
 }, {
     "room_number": 2,
@@ -20,37 +51,21 @@ rooms = [{
     "availability": True
 }]
 
+add_room(rooms, 5, "Queen", True)
+add_room(rooms, 6)
+add_room(rooms, 1)
+print(rooms)
 
-#Task1
-def add_room(rooms, room_number, bed_type = "Double", smoking=False):
-  for room in rooms:
-    if (room["room_number"] == room_number):
-      room["bed_type"] = bed_type
-      room["smoking"] = smoking
-      return
-  rooms.append({
-      "room_number": room_number,
-      "bed_type": bed_type,
-      "smoking": smoking,
-      "availability": True
-  })
+print()
+print()
 
+print(book_room(rooms))
+print(book_room(rooms))
+print(book_room(rooms))
+print(book_room(rooms, "Queen", True))
 
-#Task2
-def book_room(rooms,
-              preferred_bed_type="Double",
-              smoking_preference=False):
-  for room in rooms:
-    if (room["bed_type"] == preferred_bed_type
-        and room["smoking"] == smoking_preference):
-      if (room["availability"]):
-        room["availability"] = False
-        return "Room booked successfully"
-      else:
-        return "No rooms avaliable"
-  return "room does not exist"
+print(rooms)
 
-
-#Task3
-def list_available_rooms(rooms):
-  return [room for room in rooms if room["availability"]]
+print()
+print()
+print(list_available_rooms(rooms))

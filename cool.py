026f7fb6167365db53fde72aa1656c03a6e6ed9f
Rashay Daya <101921464+Rashay01@users.noreply.github.com>
2024-02-26 07:07:57 +0000

    cool

--- cool.py
+++ cool.py
@@ -1,10 +1,14 @@
 
 #Utility functions
 def to_upper_case(text):
+  """
+  This is a method will uppercase all the characters."""
   return text.upper()
 
 def to_lower_case(text):
   return text.lower()
+
+help(to_upper_case)
 
 # dunder variables -> __variable__
 if __name__ == "__main__":

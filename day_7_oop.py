ceb0acafd1d40b979077100cd47bcffb2c8f7de0
Rashay01 <rashay.jcdaya@gmail.com>
2024-02-27 15:05:12 +0200

    OPP Exercise

---
+++ day_7_oop.py
@@ -0,0 +1,112 @@
+# Object Orientated Programming?
+
+
+# Car
+# 1. engine
+# 2. wheels
+# 3. doors
+# 4. name
+
+# Car -> ferrari
+# 1. engine - v8
+# 2. wheels - 4
+# 3. doors  - 2
+
+# Car -> Toyata Tazz
+# 1. engine - v4
+# 2. wheels - 4
+# 3. doors  - 4
+
+#Bad way
+# class Car:
+#     pass
+
+# ferrari = Car()
+# ferrari.name = "Ferrari"
+# ferrari.engine = "v4"
+# ferrari.wheels = 4
+# ferrari.doors = 2
+
+# toyata = Car()
+# toyata.name = "Ferrari"
+# toyata.engine = "v4"
+# toyata.wheels = 4
+# toyata.doors = 2
+
+# Car --> Blueprint | Class blueprint object
+# DRY
+class Car:
+    def __init__(self, name, engine, wheels, doors):
+        self.name = name
+        self.engine = engine
+        self.wheels = wheels
+        self.doors = doors
+
+    def horn(self):
+        return "Vroom Vroom"
+
+# self --> to the object created
+# ferrari --> object
+
+ferrari = Car("Ferrari","v4", 4, 2)
+toyota = Car("Toyota", "v4", 4, 4)
+audi = Car("Audi","v4", 4, 2)
+MercedesBenz = Car("Mercedes-Benz", "v4", 4, 4)
+
+print(ferrari.name,ferrari.wheels)
+print(toyota.name,toyota.wheels)
+print(ferrari.horn())
+print(toyota.horn())
+
+# Bank Account
+# 1. account no
+# 2. name
+# 3. balance
+
+class  Bank:
+    def __init__(self, acc_no, name, balance):
+        self.acc_no = acc_no
+        self.name = name
+        self.balance = balance
+
+    def display_balance(self):
+        return f"Your balance is: R{self.balance:,}"
+
+    def withdraw(self, amount):
+        if (self.balance> amount):
+            self.balance -= amount
+            return f"Success. Your Balance is: R{self.balance:,}"
+        else:
+            return f"Unsuccess. Your Balance is: R{self.balance:,}"
+
+    def deposit(self, amount):
+        self.balance += amount
+        return f"Success. Your Balance is: R{self.balance:,}" 
+
+gemma = Bank(123, "Gemma Porrill", 15_000)
+dhara = Bank(124, "Dhara Kara", 50_001)
+caleb = Bank(125, "Caleb Potts", 100_000)
+
+#Task 2
+
+#gemma.display_balance() --> Your balance is: R15,000
+print(gemma.display_balance())
+print(dhara.display_balance())
+print(caleb.display_balance())
+
+
+# Task 3
+#caleb.withdraw(2000)
+
+print(caleb.withdraw(2000))
+print(caleb.display_balance())
+
+# Task 4
+# dhara.deposit(10_000) --> Success. Your Balance is: R60,001
+
+
+# Assignment - Transactions Tomorrow
+# #  id   Date       Type     Amount  
+# 1.  1  29 Feb   withdraw       2000
+# 2.  2  1 Mar    deposit        6000
+# 3.  3  3 Mar    deposit        7000  

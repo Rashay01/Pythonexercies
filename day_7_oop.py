a88dea9b05515a11bb6ae117f69131f63974f8f1
Rashay01 <rashay.jcdaya@gmail.com>
2024-02-28 08:24:17 +0200

    formatting fix

--- day_7_oop.py
+++ day_7_oop.py
@@ -1,4 +1,5 @@
-from datetime import datetime 
+from datetime import datetime
+
 # Object Orientated Programming?
 
 
@@ -18,7 +19,7 @@ from datetime import datetime
 # 2. wheels - 4
 # 3. doors  - 4
 
-#Bad way
+# Bad way
 # class Car:
 #     pass
 
@@ -34,6 +35,7 @@ from datetime import datetime
 # toyata.wheels = 4
 # toyata.doors = 2
 
+
 # Car --> Blueprint | Class blueprint object
 # DRY
 class Car:
@@ -46,16 +48,17 @@ class Car:
     def horn(self):
         return "Vroom Vroom"
 
+
 # self --> to the object created
 # ferrari --> object
 
-ferrari = Car("Ferrari","v4", 4, 2)
+ferrari = Car("Ferrari", "v4", 4, 2)
 toyota = Car("Toyota", "v4", 4, 4)
-audi = Car("Audi","v4", 4, 2)
+audi = Car("Audi", "v4", 4, 2)
 MercedesBenz = Car("Mercedes-Benz", "v4", 4, 4)
 
-print(ferrari.name,ferrari.wheels)
-print(toyota.name,toyota.wheels)
+print(ferrari.name, ferrari.wheels)
+print(toyota.name, toyota.wheels)
 print(ferrari.horn())
 print(toyota.horn())
 
@@ -82,21 +85,25 @@ print(toyota.horn())
 
 #     def deposit(self, amount):
 #         self.balance += amount
-#         return f"Success. Your Balance is: R{self.balance:,}" 
+#         return f"Success. Your Balance is: R{self.balance:,}"
 
-class  Bank:
+# -----------------------------------------------------------------------------
+# PEP 8 - Python Enchancements proposals
+
+
+class Bank:
     def __init__(self, acc_no, name, balance):
         self.acc_no = acc_no
         self.name = name
         self.balance = balance
-        self.transactions =[]
+        self.transactions = []
         self.create_transaction("deposit", balance)
 
     def display_balance(self):
         return f"Your balance is: R{self.balance:,}"
 
     def withdraw(self, amount):
-        if (self.balance> amount):
+        if self.balance > amount:
             self.balance -= amount
             self.create_transaction("withdraw", amount)
             return f"Success. Your Balance is: R{self.balance:,}"
@@ -107,34 +114,41 @@ class  Bank:
         self.balance += amount
         self.create_transaction("deposit", amount)
         return f"Success. Your Balance is: R{self.balance:,}"
-        
+
     def create_transaction(self, type, amount):
-        if (len(self.transactions) > 0):
-            id = self.transactions[-1].get('id',0) + 1
-        else: 
+        if len(self.transactions) > 0:
+            id = self.transactions[-1].get("id", 0) + 1
+        else:
             id = 1
-        self.transactions.append({"id" : id, "Date" : datetime.now(), "Type" : type, "Amount": amount})
-    
+        self.transactions.append(
+            {"id": id, "Date": datetime.now(), "Type": type, "Amount": amount}
+        )
+
     def print_transactions(self):
         statment = f"{'id':>6} {'Date':>5} {'Type':>6} {'Amount':>10}"
-        for i,transaction in enumerate(self.transactions):
-            statment = statment + f"\n {i}. {transaction['id']:^3} {transaction['Date']:%d %b} {transaction['Type']:^8} {transaction['Amount']:<7,}"
+        for i, transaction in enumerate(self.transactions):
+            statment = (
+                statment
+                + f"\n {i}. {transaction['id']:^3} {transaction['Date']:%d %b}"
+                + f" {transaction['Type']:^8} {transaction['Amount']:<7,}"
+            )
         return statment
+
 
 gemma = Bank(123, "Gemma Porrill", 15_000)
 dhara = Bank(124, "Dhara Kara", 50_001)
 caleb = Bank(125, "Caleb Potts", 100_000)
 
-#Task 2
+# Task 2
 
-#gemma.display_balance() --> Your balance is: R15,000
+# gemma.display_balance() --> Your balance is: R15,000
 print(gemma.display_balance())
 print(dhara.display_balance())
 print(caleb.display_balance())
 
 
 # Task 3
-#caleb.withdraw(2000)
+# caleb.withdraw(2000)
 
 print(caleb.withdraw(2_000))
 print(caleb.display_balance())
@@ -145,25 +159,25 @@ print(dhara.deposit(10_000))
 
 
 # Assignment - Transactions Tomorrow
-# #  id   Date       Type     Amount  
+# #  id   Date       Type     Amount
 # 1.  1  29 Feb   withdraw       2000
 # 2.  2  1 Mar    deposit        6000
-# 3.  3  3 Mar    deposit        7000  
+# 3.  3  3 Mar    deposit        7000
 
 print(caleb.print_transactions())
 
 
 caleb1 = Bank(125, "Caleb Potts", 100_000)
-print('withdraw R2,000')
+print("withdraw R2,000")
 print(caleb1.withdraw(2_000))
 
-print('deposit R4,000')
+print("deposit R4,000")
 print(caleb1.deposit(4_000))
 
-print('deposit R10,000')
+print("deposit R10,000")
 print(caleb1.deposit(10_000))
 
-print('withdraw R6,000')
+print("withdraw R6,000")
 print(caleb1.withdraw(6_000))
 
 print()
--- day_7_scope.py
+++ day_7_scope.py
@@ -1,8 +1,7 @@
+# Vs code shortcut crt + shift + p -> command
+# ctrl + p -> find file
 
-#Vs code shortcut crt + shift + p -> command
-#ctrl + p -> find file
-
-# Lexical scope 
+# Lexical scope
 # def market():
 #     price = 100
 #     def get_price():
@@ -11,7 +10,7 @@
 
 # market()
 
-#Scope: Lifetime of a variable 
+# Scope: Lifetime of a variable
 # def cool():
 #     x = 10              # x lifeline is till the end of function
 #     print('super')
@@ -22,27 +21,31 @@
 
 code_word = "Hulk"
 
+
 def space_ship():
-    question ="please provide code word"
+    question = "please provide code word"
 
     def code_word_check():
         password = "Hulk"
         print(question)
 
-        if(password == code_word):
+        if password == code_word:
             print(f"Welcome, {password} the strongest avenger")
         else:
             print("Access denied")
+
     code_word_check()
 
-space_ship()
 
+space_ship()
 
 
 def add(x):
     def add1(y):
         return x + y
+
     return add1
+
 
 print(add(10)(5))
 
@@ -53,16 +56,19 @@ def fun(nums=[]):
     nums.append(10)
     print(nums)
 
+
 fun()
 fun()
 fun()
 fun()
 
-def fun(nums = None):
-    if (nums is None):
-        nums =[]
+
+def fun(nums=None):
+    if nums is None:
+        nums = []
     nums.append(10)
     print(nums)
+
 
 fun()
 fun()
@@ -71,5 +77,5 @@ fun()
 
 a = [1, 2, 3]
 b = [1, 2, 3]
-print(a is b) # False, checks memory address
-print(a == b) # checks values
+print(a is b)  # False, checks memory address
+print(a == b)  # checks values

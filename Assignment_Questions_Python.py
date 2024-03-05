49a399cb164693dce7eb899703b0fb4c96e64072
Rashay01 <rashay.jcdaya@gmail.com>
2024-03-05 19:15:20 +0200

    Day12

--- Assignment_Questions_Python.py
+++ Assignment_Questions_Python.py
@@ -1,4 +1,6 @@
 # Q1
+print("Q1")
+
 
 students = [
     {"name": "Alice", "grade": 88},
@@ -12,9 +14,10 @@ for i, student in enumerate(sorted_students):
     student["rank"] = i + 1
 print(sorted_students)
 
-
+# =========================================================================================================================================
+print()
+print("Q2")
 # Q2
-
 # Setup Code
 employees = [{"id": 1, "name": "Alice"}, {"id": 2, "name": "Bob"}]
 salaries = [{"id": 1, "salary": 50000}, {"id": 2, "salary": 60000}]
@@ -23,7 +26,9 @@ salaries = [{"id": 1, "salary": 50000}, {"id": 2, "salary": 60000}]
 merged_list = [{**x, **y} for x, y in zip(employees, salaries)]
 print(merged_list)
 
-
+# =========================================================================================================================================
+print()
+print("Q3")
 # Q3
 # Setup Code
 products = [
@@ -44,6 +49,10 @@ filtered_products = filter(
 )
 print(list(filtered_products))
 
+
+# =========================================================================================================================================
+print()
+print("Q4")
 # Q4
 # Setup Code
 orders = [
@@ -68,6 +77,10 @@ for order in orders:
 
 print(quattity_products)
 
+
+# =========================================================================================================================================
+print()
+print("Q5")
 # Q5
 # Setup Code
 transactions = [
@@ -85,6 +98,10 @@ for transaction in transactions:
 
 print(product_totals)
 
+
+# =========================================================================================================================================
+print()
+print("Q6")
 # Q6
 # Setup Code
 sales = [
@@ -102,6 +119,9 @@ for sale in sales:
 
 print(grouped_sales_data)
 
+# =========================================================================================================================================
+print()
+print("Q7")
 # 7
 # Setup Code
 spells = [("Lumos", 5), ("Obliviate", 10), ("Expelliarmus", 7)]
@@ -111,6 +131,9 @@ sorted_spells = sorted(spells, key=lambda spell: spell[1], reverse=True)
 
 print(sorted_spells)
 
+# =========================================================================================================================================
+print()
+print("Q8")
 # 8
 # Setup Code
 ingredients = ["Wolfsbane", "Eye of Newt", "Dragon Scale"]
@@ -120,6 +143,9 @@ ingredients_3grams = map(lambda x: x + ": 3 grams", ingredients)
 
 print(list(ingredients_3grams))
 
+# =========================================================================================================================================
+print()
+print("Q9")
 # 9
 # Setup Code
 books = [
@@ -132,10 +158,9 @@ filtered_books = filter(lambda book: book["pages"] > 120, books)
 book_names = map(lambda book: book["title"].upper(), filtered_books)
 print(list(book_names))
 
-# 10
-# Setup Code
-# Expected Task: Implement a class with methods for casting spells, reducing health points, and determining the winner.
-
+# =========================================================================================================================================
+print()
+print("Q10")
 # 10
 # Setup Code
 # Expected Task: Implement a class with methods for casting spells, reducing health points, and determining the winner.
@@ -181,7 +206,9 @@ print(draco.determin_winner(harry))
 harry.cast_spell("fireball", draco)
 print(harry.determin_winner(draco))
 
-
+# =========================================================================================================================================
+print()
+print("Q11")
 # 11
 # Setup Code
 # Expected Task: Define a `PotionError` exception and use it in a potion-making function.
@@ -210,7 +237,9 @@ def potion_making(ingredient_1, ingredient_2):
 potion_making("frog", "grass")
 potion_making("grass", "grass")
 
-
+# =========================================================================================================================================
+print()
+print("Q12")
 # 12
 # Setup Code
 library = [
@@ -224,6 +253,9 @@ books_by_author = [book for book in library if book["author"] == author]
 
 print(books_by_author)
 
+# =========================================================================================================================================
+print()
+print("Q13")
 # 13
 # Setup Code
 house_points = [
@@ -246,6 +278,9 @@ for house, points in total_house_points.items():
 
 print(output[:-2])
 
+# =========================================================================================================================================
+print()
+print("Q14")
 # 14
 # Setup Code
 # Expected Task: Create a base class `MagicalCreature` and subclasses `Dragon`, `Unicorn`. Each subclass should have a unique `sound` method.
@@ -276,7 +311,9 @@ print("Draco says:", draco.sound())
 unicorn = Unicorn("Unicorn")
 print("Unicorn says:", unicorn.sound())
 
-
+# =========================================================================================================================================
+print()
+print("Q15")
 # 15
 # Setup Code
 artifacts = [
@@ -291,6 +328,10 @@ sorted_artifacts = sorted(artifacts, key=lambda x: (x["age"], x["power"]))
 
 print(sorted_artifacts)
 
+
+# =========================================================================================================================================
+print()
+print("Q16")
 # 16
 # Setup Code
 wizard = {"name": "Albus Dumbledore", "title": "Headmaster", "house": "Gryffindor"}
@@ -306,7 +347,9 @@ def profile(wizard):
 
 print(profile(wizard))
 
-
+# =========================================================================================================================================
+print()
+print("Q17")
 # 17
 # Setup Code
 adopters = [("Harry", "Phoenix"), ("Hermione", "House Elf")]
@@ -331,6 +374,9 @@ def get_matches(name):
 print(list(map(get_matches, filter(isin, creatures))))
 
 
+# =========================================================================================================================================
+print()
+print("Q18")
 # 18
 # Setup Code
 ingredients = ["Moonstone", "Silver Dust", "Dragon Blood"]
@@ -340,9 +386,12 @@ for i in range(len(ingredients) - 1):
     for j in range(i + 1, len(ingredients)):
         if i != j:
             print(
-                f"Combining {ingredients[i]} and {ingredients[j]} produces a Visibility Potion."
+                f"Combining {ingredients[i]} and {ingredients[j]} produces a {ingredients[i]} {ingredients[j]} Potion."
             )
 
+# =========================================================================================================================================
+print()
+print("Q19")
 # 19
 # Setup Code
 data = [
@@ -363,6 +412,9 @@ add_tag(data)
 
 print(data)
 
+# =========================================================================================================================================
+print()
+print("Q20")
 # 20
 # Setup Code
 tasks = [
